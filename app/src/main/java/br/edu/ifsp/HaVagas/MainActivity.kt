735dc6181f75c02704e4ad7bcc15197c0e4d71c9
gabrmanh <gmanhani@yahoo.com>
2024-08-25 17:45:11 -0300

    feat: showFields function to show fields depending on school level

--- app/src/main/java/br/edu/ifsp/HaVagas/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/HaVagas/MainActivity.kt
@@ -27,12 +27,59 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        amb.formacaoRg.setOnCheckedChangeListener { _, checkedId ->
+            showFields(checkedId)
+        }
+
         amb.limparBt.setOnClickListener {
             limpar()
         }
 
         amb.salvarBt.setOnClickListener {
 
+        }
+    }
+
+    private fun showFields(checkedId: Int) {
+        when (checkedId) {
+            R.id.fundamentalRb, R.id.medioRb -> {
+                amb.anoFormaturaTv.visibility = View.VISIBLE
+                amb.anoFormaturaEt.visibility = View.VISIBLE
+                amb.anoConclusaoTv.visibility = View.GONE
+                amb.anoConclusaoEt.visibility = View.GONE
+                amb.instituicaoTv.visibility = View.GONE
+                amb.instituicaoEt.visibility = View.GONE
+                amb.monografiaTv.visibility = View.GONE
+                amb.monografiaEt.visibility = View.GONE
+                amb.orientadorTv.visibility = View.GONE
+                amb.orientadorEt.visibility = View.GONE
+            }
+
+            R.id.superiorRb, R.id.especializacaoRb -> {
+                amb.anoFormaturaTv.visibility = View.GONE
+                amb.anoFormaturaEt.visibility = View.GONE
+                amb.anoConclusaoTv.visibility = View.VISIBLE
+                amb.anoConclusaoEt.visibility = View.VISIBLE
+                amb.instituicaoTv.visibility = View.VISIBLE
+                amb.instituicaoEt.visibility = View.VISIBLE
+                amb.monografiaTv.visibility = View.GONE
+                amb.monografiaEt.visibility = View.GONE
+                amb.orientadorTv.visibility = View.GONE
+                amb.orientadorEt.visibility = View.GONE
+            }
+
+            R.id.mestradoRb, R.id.doutoradoRb -> {
+                amb.anoFormaturaTv.visibility = View.GONE
+                amb.anoFormaturaEt.visibility = View.GONE
+                amb.anoConclusaoTv.visibility = View.VISIBLE
+                amb.anoConclusaoEt.visibility = View.VISIBLE
+                amb.instituicaoTv.visibility = View.VISIBLE
+                amb.instituicaoEt.visibility = View.VISIBLE
+                amb.monografiaTv.visibility = View.VISIBLE
+                amb.monografiaEt.visibility = View.VISIBLE
+                amb.orientadorTv.visibility = View.VISIBLE
+                amb.orientadorEt.visibility = View.VISIBLE
+            }
         }
     }
 
