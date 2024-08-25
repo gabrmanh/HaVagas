c0c258e5aff620381f5ca825856d76bbb043c008
gabrmanh <gmanhani@yahoo.com>
2024-08-25 17:16:46 -0300

    refactor: initial MainActivity class

--- app/src/main/java/br/edu/ifsp/HaVagas/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/HaVagas/MainActivity.kt
@@ -1,20 +1,40 @@
 package br.edu.ifsp.HaVagas
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.HaVagas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var amb: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        amb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(amb.root)
+
+        val emailList = mutableListOf<String>()
+
+        amb.limparBt.setOnClickListener {
+            limpar()
         }
+
+        amb.salvarBt.setOnClickListener {
+
+        }
+    }
+
+    private fun limpar() {
+        amb.nomeEt.text.clear()
+        amb.telefoneEt.text.clear()
+        amb.emailEt.text.clear()
+        if (amb.emailCb.isChecked) amb.emailCb.toggle()
+        amb.generoRg.clearCheck()
+        amb.cidadeEt.text.clear()
     }
 }
