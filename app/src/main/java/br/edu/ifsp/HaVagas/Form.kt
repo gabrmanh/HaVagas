9a6b93ddbe2b50b3410625862664a8c8be2a4d4b
gabrmanh <gmanhani@yahoo.com>
2024-08-25 22:48:20 -0300

    feat: form class

---
+++ app/src/main/java/br/edu/ifsp/HaVagas/Form.kt
@@ -0,0 +1,22 @@
+package br.edu.ifsp.HaVagas
+
+import java.util.Date
+
+class Form(
+    var name: String?,
+    var email: String?,
+    var phone: String?,
+    var cellphone: String?,
+    var gender: String,
+    var birthdate: Date?,
+    var schoolLevel: String?,
+    var graduationYear: Int?,
+    var conclusionYear: Int?,
+    var institution: String?,
+    var monography: String?,
+    var tutor: String?,
+    var positions: String?
+) {
+
+
+}
