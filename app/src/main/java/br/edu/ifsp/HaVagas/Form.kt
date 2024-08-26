9fe878407ad6011d7206dd2ea27b0b450281aa88
gabrmanh <gmanhani@yahoo.com>
2024-08-25 23:16:44 -0300

    feat: mailing list var

--- app/src/main/java/br/edu/ifsp/HaVagas/Form.kt
+++ app/src/main/java/br/edu/ifsp/HaVagas/Form.kt
@@ -7,7 +7,7 @@ class Form(
     var email: String?,
     var phone: String?,
     var cellphone: String?,
-    var gender: String,
+    var gender: String?,
     var birthdate: Date?,
     var schoolLevel: String?,
     var graduationYear: Int?,
@@ -15,8 +15,11 @@ class Form(
     var institution: String?,
     var monography: String?,
     var tutor: String?,
-    var positions: String?
+    var positions: String?,
+    var mailingList: Boolean
 ) {
 
-
+    override fun toString(): String {
+        return "Form(name=$name, email=$email, phone=$phone, cellphone=$cellphone, gender=$gender, birthdate=$birthdate, schoolLevel=$schoolLevel, graduationYear=$graduationYear, conclusionYear=$conclusionYear, institution=$institution, monography=$monography, tutor=$tutor, positions=$positions)"
+    }
 }
